3c3981e6d3138d5d60baa050c89953923cdbb60d
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2024-03-13 16:20:38 +0530

    Create Version1.cs

---
+++ Version1.cs
@@ -0,0 +1,37 @@
+
+using System;
+
+string secretWord = "Stark";
+
+string guess = "";
+
+int guessCount = 0;
+int guessLimit = 3;
+bool outOfGuesses = false;
+
+
+while(guess != secretWord && !outOfGuesses)
+{
+    if(guessCount < guessLimit)
+    {
+        Console.Write("Enter guess: ");
+        guess = Console.ReadLine();
+        guessCount++;
+    }
+    else
+    {
+        outOfGuesses = true;
+    }
+}
+
+if(outOfGuesses)
+{
+    Console.Write("You Lose!!");
+}
+else
+{
+    Console.Write("You Win!!");
+}
+
+
+Console.ReadLine();
